fa6ca23d0b75ea28177ed5a083007ea2128c3280
Fly Dev <dev@fly.io>
2025-07-05 13:35:18 +0000

    create seed data with test user

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+import Ecto.Query
+alias FinancialAdvisorAi.{Repo, Accounts}
+
+# Create test user for OAuth testing
+email = "webshookeng@gmail.com"
+password = "password123456"
+
+# Check if user already exists
+existing_user = Repo.one(from u in Accounts.User, where: u.email == ^email)
+
+if existing_user do
+  IO.puts("Test user #{email} already exists")
+else
+  {:ok, user} = Accounts.register_user(%{email: email})
+  {:ok, user, _expired_tokens} = Accounts.update_user_password(user, %{password: password})
+  IO.puts("Created test user: #{email} with password: #{password}")
+end

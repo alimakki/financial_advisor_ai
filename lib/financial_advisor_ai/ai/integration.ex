4b106b454f13f4efe2a47d5505579f69e31a4106
Fly Dev <dev@fly.io>
2025-07-05 13:33:31 +0000

    create integration schema

---
+++ lib/financial_advisor_ai/ai/integration.ex
@@ -0,0 +1,33 @@
+defmodule FinancialAdvisorAi.AI.Integration do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "integrations" do
+    field :provider, :string
+    field :access_token, :string
+    field :refresh_token, :string
+    field :expires_at, :utc_datetime
+    field :scope, :string
+    field :metadata, :map, default: %{}
+
+    belongs_to :user, FinancialAdvisorAi.Accounts.User
+
+    timestamps(type: :utc_datetime)
+  end
+
+  def changeset(integration, attrs) do
+    integration
+    |> cast(attrs, [
+      :provider,
+      :access_token,
+      :refresh_token,
+      :expires_at,
+      :scope,
+      :metadata,
+      :user_id
+    ])
+    |> validate_required([:provider, :user_id])
+    |> validate_inclusion(:provider, ["google", "hubspot"])
+    |> unique_constraint([:user_id, :provider])
+  end
+end
